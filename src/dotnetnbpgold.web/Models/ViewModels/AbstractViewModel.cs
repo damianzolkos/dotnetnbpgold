eae9aaabe8f27f3a7fc8ec67f8424dadd48b135e
Damian Żółkoś <damianzolkos@gmail.com>
2022-07-26 18:26:52 +0200

    Style changes, icon added, credits added.

---
+++ src/dotnetnbpgold.web/Models/ViewModels/AbstractViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetnbpgold.web.Models.ViewModels
+{
+    public abstract class AbstractViewModel
+    {
+        public string ErrorMessage { get; set; }
+    }
+}
--- src/dotnetnbpgold.web/Models/ViewModels/GoldPriceViewModel.cs
+++ src/dotnetnbpgold.web/Models/ViewModels/GoldPriceViewModel.cs
@@ -6,7 +6,7 @@ using dotnetnbpgold.web.Models.DTOs;
 
 namespace dotnetnbpgold.web.Models.ViewModels
 {
-    public class GoldPriceViewModel
+    public class GoldPriceViewModel : AbstractViewModel
     {
         public DatePriceDTO StartDateGoldPrice { get; set; }
         public DatePriceDTO EndDateGoldPrice { get; set; }

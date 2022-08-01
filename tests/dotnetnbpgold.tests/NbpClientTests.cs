91bf00026c9e86a331f1aea0bf8c2ded84eae3a1
Damian Żółkoś <damianzolkos@gmail.com>
2022-08-01 19:17:44 +0200

    Basic tests added.

--- tests/dotnetnbpgold.tests/NbpClientTests.cs
+++ tests/dotnetnbpgold.tests/NbpClientTests.cs
@@ -1,10 +1,17 @@
 namespace dotnetnbpgold.tests;
+using dotnetnbpgold.nbp.client;
+using dotnetnbpgold.nbp.client.Models.Settings;
+using Microsoft.Extensions.Options;
 
 public class NbpClientTests
 {
+    private readonly DotNetNBPGoldClient _sut;
     public NbpClientTests()
     {
-        
+        DotNetNBPGoldClientSettings mockOptions = new DotNetNBPGoldClientSettings() { };
+        IOptions<DotNetNBPGoldClientSettings> options = Options.Create(mockOptions);
+
+        _sut = new DotNetNBPGoldClient(options);
     }
 
     [Fact]
@@ -12,10 +19,63 @@ public class NbpClientTests
     {
         // Arrange.
         var temp = true;
-        
+        DateTime startDate = new DateTime(2022, 1, 1);
+        DateTime endDate = new DateTime(2022, 1, 1);
+
         // Act.
 
         // Assert.
-        temp.Should().Be(true);
+    }
+
+    [Fact]
+    public void DotNetNBPGoldClient_ShouldThrow_Exception_When_EndDateIsInTheFuture()
+    {
+        // Arrange.
+        var temp = true;
+        DateTime startDate = new DateTime(2022, 1, 1);
+        DateTime endDate = new DateTime(2022, 1, 1);
+
+        // Act.
+
+        // Assert.
+    }
+
+    [Fact]
+    public void DotNetNBPGoldClient_ShouldThrow_Exception_When_EndDateIsBeforeStartDate()
+    {
+        // Arrange.
+        var temp = true;
+        DateTime startDate = new DateTime(2022, 1, 1);
+        DateTime endDate = new DateTime(2022, 1, 1);
+
+        // Act.
+
+        // Assert.
+    }
+
+    [Fact]
+    public void DotNetNBPGoldClient_ShouldThrow_Exception_When_EndDateIsBeforeMinimumDate()
+    {
+        // Arrange.
+        var temp = true;
+        DateTime startDate = new DateTime(2022, 1, 1);
+        DateTime endDate = new DateTime(2022, 1, 1);
+
+        // Act.
+
+        // Assert.
+    }
+
+        [Fact]
+    public void DotNetNBPGoldClient_ShouldThrow_Exception_When_PeriodIsLongerThanMaximumPeriodDays()
+    {
+        // Arrange.
+        var temp = true;
+        DateTime startDate = new DateTime(2022, 1, 1);
+        DateTime endDate = new DateTime(2022, 1, 1);
+
+        // Act.
+
+        // Assert.
     }
 }

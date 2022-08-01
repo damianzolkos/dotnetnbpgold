6c9454fa913874c8019241f18c48512a1f34b475
Damian Żółkoś <damianzolkos@gmail.com>
2022-08-01 20:11:33 +0200

    Test boilerplates fixes.

--- tests/dotnetnbpgold.tests/NbpClientTests.cs
+++ tests/dotnetnbpgold.tests/NbpClientTests.cs
@@ -19,9 +19,8 @@ public class NbpClientTests
     public void DotNetNBPGoldClient_ShouldThrow_Exception_When_StartDateIsInTheFuture()
     {
         // Arrange.
-        var temp = true;
-        DateTime startDate = new DateTime(2022, 1, 1);
-        DateTime endDate = new DateTime(2022, 1, 1);
+        DateTime startDate = DateTime.Now.AddDays(10);
+        DateTime endDate = DateTime.Now.AddDays(5);
 
         // Act.
 
@@ -32,9 +31,8 @@ public class NbpClientTests
     public void DotNetNBPGoldClient_ShouldThrowException_When_EndDateIsInTheFuture()
     {
         // Arrange.
-        var temp = true;
-        DateTime startDate = new DateTime(2022, 1, 1);
-        DateTime endDate = new DateTime(2022, 1, 1);
+        DateTime startDate = DateTime.Now.AddDays(-10);
+        DateTime endDate = DateTime.Now.AddDays(5);
 
         // Act.
 
@@ -45,9 +43,8 @@ public class NbpClientTests
     public void DotNetNBPGoldClient_ShouldThrowException_When_EndDateIsBeforeStartDate()
     {
         // Arrange.
-        var temp = true;
-        DateTime startDate = new DateTime(2022, 1, 1);
-        DateTime endDate = new DateTime(2022, 1, 1);
+        DateTime startDate = DateTime.Now.AddDays(-10);
+        DateTime endDate = DateTime.Now.AddDays(-20);
 
         // Act.
 
@@ -58,9 +55,8 @@ public class NbpClientTests
     public void DotNetNBPGoldClient_ShouldThrowException_When_EndDateIsBeforeMinimumDate()
     {
         // Arrange.
-        var temp = true;
-        DateTime startDate = new DateTime(2022, 1, 1);
-        DateTime endDate = new DateTime(2022, 1, 1);
+        DateTime startDate = DateTime.Now.AddDays(-10);
+        DateTime endDate = new DateTime(1990, 1, 1);
 
         // Act.
 
@@ -71,9 +67,8 @@ public class NbpClientTests
     public void DotNetNBPGoldClient_ShouldThrowException_When_PeriodIsLongerThanMaximumPeriodDays()
     {
         // Arrange.
-        var temp = true;
-        DateTime startDate = new DateTime(2022, 1, 1);
-        DateTime endDate = new DateTime(2022, 1, 1);
+        DateTime startDate = DateTime.Now.AddDays(-91);
+        DateTime endDate = DateTime.Now;
 
         // Act.
 
@@ -84,9 +79,8 @@ public class NbpClientTests
     public void DotNetNBPGoldClient_ShouldThrowException_When_NbpApiReturnsNot200StatusCode()
     {
         // Arrange.
-        var temp = true;
-        DateTime startDate = new DateTime(2022, 1, 1);
-        DateTime endDate = new DateTime(2022, 1, 1);
+        DateTime startDate = DateTime.Now.AddDays(-10);
+        DateTime endDate = DateTime.Now;
 
         // Act.
 
@@ -97,9 +91,8 @@ public class NbpClientTests
     public void DotNetNBPGoldClient_ShouldReturn_ListOfNBPGoldDatePriceResponse()
     {
         // Arrange.
-        var temp = true;
-        DateTime startDate = new DateTime(2022, 1, 1);
-        DateTime endDate = new DateTime(2022, 1, 1);
+        DateTime startDate = DateTime.Now.AddDays(-10);
+        DateTime endDate = DateTime.Now;
 
         // Act.
 
